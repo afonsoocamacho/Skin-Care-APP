f5afa6adaf262c93697af5510209bff18550ec2e
afonsoocamacho <afonsocamacho62@gmail.com>
2025-01-19 21:26:12 +0100

    lots of changes

---
+++ Skin Care/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Skin Care
+//
+//  Created by Afonso Camacho on 19/01/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
